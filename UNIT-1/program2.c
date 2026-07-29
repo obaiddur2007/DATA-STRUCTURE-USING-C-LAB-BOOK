53387a0f0ac0b4c6a7ee0f6f04a7d952635e1f57
OBAIDDUR RAHMAN AJAJI <obaiddur2007@gmail.com>
2026-07-29 22:11:01 +0530

    Add files via upload

---
+++ UNIT-1/program2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10], i, sum = 0;
+    float avg;
+
+    printf("Enter 10 elements:\n");
+
+    for(i = 0; i < 10; i++)
+    {
+        scanf("%d", &a[i]);
+        sum = sum + a[i];
+    }
+
+    avg = sum / 10.0;
+
+    printf("\nSum = %d", sum);
+    printf("\nAverage = %.2f", avg);
+
+    return 0;
+}
